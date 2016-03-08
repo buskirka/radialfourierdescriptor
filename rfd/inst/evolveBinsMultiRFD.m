fbb44e5aabd313f2dff983329f67786ff9d56a43
Adam Buskirk <adam.buskirk@sdstate.edu>
2016-03-07 18:12:24 -0600

    0.1.0

--- rfd/inst/evolveBinsMultiRFD.m
+++ rfd/inst/evolveBinsMultiRFD.m
@@ -84,12 +84,14 @@ for i=1:size(binEcosystem,1)
                 if( max(max(tr{imscan}==255)) )
     				truePos{i,j,imscan}=mean( svlp{imscan}(pos) == predlab(pos) ); 
                 else
+                    printf(['No class 255 in ',num2str(imscan),'\n'])
                     truePos{i,j,imscan}=1;
                 endif
 				neg=find(svlp{imscan}==128);
                 if( max(max(tr{imscan}==128)) )
 				    trueNeg{i,j,imscan}=mean( svlp{imscan}(neg) == predlab(neg) );
                 else
+                    printf(['No class 128 in ',num2str(imscan),'\n'])
                     trueNeg{i,j,imscan}=1;
                 endif
 				printf([num2str(sqrt(truePos{i,j,imscan}*trueNeg{i,j,imscan})),'\n']);
