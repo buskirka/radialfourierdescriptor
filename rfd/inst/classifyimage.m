c98546251e5fc1a0df7bf633b231ccd04018d21e
Adam Buskirk <adam.buskirk@sdstate.edu>
2016-04-25 23:30:53 -0500

    Adding Training Data tarball

--- rfd/inst/classifyimage.m
+++ rfd/inst/classifyimage.m
@@ -1,29 +1,39 @@
-function classy = classifyimage(svm,DataCells,binConfig)
+function classy = classifyimage(svm,DataCells,binConfig,pics=false)
 	svl=[]; 
 	svd=[]; 
     classy=[];
 	fflush(stdout); 
-	for i=200:200:(size(DataCells{1},1)-200); 
-		for j=200:200:(size(DataCells{1},2)-200); 
-            [~,sizes]=neighborhood(DataCells{1},[i,j],100);
+    radius=400;
+	for i=(2*radius):(2*radius):(size(DataCells{1},1)-(2*radius)); 
+		for j=(2*radius):(2*radius):(size(DataCells{1},2)-(2*radius)); 
+            [~,sizes]=neighborhood(DataCells{1},[i,j],radius);
         	pile=svmRFDExtract(
                 DataCells,
                 'rbins',binConfig.rbins,
                 'sines',binConfig.sines,
-                'radius',100,
+                'radius',radius,
                 'point',[i,j],
                 'array'); 
             classymini=[];
+            dat=[];
+            fakel=[];
             for y=1:size(pile,2)
-                dat=pile(:,y,:);
-                dat=permute(dat,[1,3,2]);
-                fakel=zeros(size(dat,1),1);
-                predline=svmpredict( double(fakel), double(dat), svm, '-q');
-                classymini=[classymini;predline'];
+                datt=pile(:,y,:);
+                datt=permute(datt,[1,3,2]);
+                fakelt=zeros(size(datt,1),1);
+                dat=[dat;datt];
+                fakel=[fakel;fakelt];
                 printf('.'); fflush(stdout);
             endfor
+            printf(' * ');
+            predline=svmpredict( double(fakel), double(dat), svm);
+            classymini=reshape(predline,size(pile,1),size(pile,2));
             classy(sizes{1},sizes{2})=classymini ;
             printf(':');
+            if(pics==true)
+                imshow(classy);
+                sleep(0.001);
+            endif
         endfor
     endfor
 endfunction
